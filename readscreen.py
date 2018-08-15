9602afac7507bea2356cdf365f9cd404863f7dca
Robur <>
2018-08-15 18:39:06 +0200

    Profiling two screengrab algorithms.

--- readscreen.py
+++ readscreen.py
@@ -5,15 +5,14 @@ from mss.windows import MSS as mss
 import mss.tools as tools
 from time import time
 
-def get_screen(x_min, y_min, x_max, y_max):
-    return ImageGrab.grab(bbox=(0,0, 1280, 960))
+def get_screen(bbox):
+    return ImageGrab.grab(bbox=bbox)
 
 def convert_arr(arr):
     """
     Converts 1d to 2d array
     """
-    return np.array(arr, dtype='uint8').reshape((arr.size[1], arr.size[0], 3))
-    
+    return np.array(arr).reshape((arr.size[1], arr.size[0], 3))
 
 def arr_to_png(arr, location):
     """
@@ -22,34 +21,44 @@ def arr_to_png(arr, location):
     scipy.misc.imsave(location, arr)
     
 
+def get_screen2(sct, monitor):
+    screenshot = sct.grab(monitor)
+    return Image.frombytes('RGB', screenshot.size, screenshot.bgra, 'raw', 'BGRX').tobytes() #convert from BGRA to RGB (mss docs zegt dat PIL versie snelste is?)
 
 
-def get_screen2(monitor):
-    # If converting this into function, remember to not instantiate a new mss() object each function call
-    with mss() as sct:
-        img = sct.grab(monitor)
-        Image.frombytes('RGB', img.size, img.bgra, 'raw', 'BGRX').tobytes() #convert from BGRA ro RGB (mss docs zegt dat PIL versie snelste is?)
-        return img
-
-
+repeat=1000
 
 start_time = time()
-arr_to_png(
-    arr=get_screen(0, 0, 1280, 960),
-    location='test.png'
-)
-print('arr_to_png took {}'.format(time() - start_time))
-
+for i in range(0, repeat):
+    get_screen((0,0, 1280, 960))
+print('get_screen     x{} took {}'.format(repeat, time() - start_time))
 
 start_time = time()
-img = get_screen2(monitor={"top": 0, "left": 0, "width": 1280, "height": 960})
-print('get_screen2 took {}'.format(time() - start_time))
-print('Saving took {}'.format(str(np.asarray(img).shape)))
+for i in range(0, repeat):
+    convert_arr(get_screen((0,0, 1280, 960)))
+print('get_screen np  x{} took {}'.format(repeat, time() - start_time))
+
+with mss() as sct:
+    start_time = time()
+    for i in range(0, repeat):
+        get_screen2(sct=sct, monitor={"top": 0, "left": 0, "width": 1280, "height": 960})
+    print('get_screen2    x{} took {}'.format(repeat, time() - start_time))
+    
+    start_time = time()
+    for i in range(0, repeat):
+        np.asarray(get_screen2(sct=sct, monitor={"top": 0, "left": 0, "width": 1280, "height": 960}))
+    print('get_screen2 np x{} took {}'.format(repeat, time() - start_time))
 
 
-start_time = time()
-output = 'test.png'
-tools.to_png(img.rgb, img.size, output=output)
-print('Saving took {}'.format(time() - start_time))
-print('Saved image: {}'.format(output))
+
+
+
+
+#arr_to_png(arr=screen1, location='test.png')
+#print('Saving took {}'.format(str(np.asarray(screen2).shape)))
+#start_time = time()
+#output = 'test.png'
+#tools.to_png(screen2.rgb, screen2.size, output=output)
+#print('Saving took {}'.format(time() - start_time))
+#print('Saved image: {}'.format(output))
 
